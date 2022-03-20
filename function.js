ed8fdd2f919de3d75615f2b561801d056ac0670c
Mike Flintrop <mike.flintrop@gmail.com>
2022-03-19 21:14:04 -0500

    Updates var to let

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 function openPage(pageName, elmnt, color) {
   // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";

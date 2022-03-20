898c07c1fea6c4e78cf7790dfac9d40979f5017d
Mike Flintrop <mike.flintrop@gmail.com>
2022-03-20 10:16:55 -0500

    Updates JS 3/20

--- function.js
+++ function.js
@@ -1,23 +1,24 @@
-function openPage(pageName, elmnt, color) {
+window.onload = function() {
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+}
+
+function displaySection(sectionId, button, color) {
   // Hide all elements with class="tabcontent" by default */
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+  let sections = document.getElementsByClassName("section");
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.display = "none";
   }
 
   // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+  let buttons = document.getElementsByClassName("tablink");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].style.backgroundColor = "";
   }
 
   // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
+  document.getElementById(sectionId).style.display = "block";
 
   // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+  button.style.backgroundColor = color;
 }
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
